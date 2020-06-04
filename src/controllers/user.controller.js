6acf90f6bcfd14f64708581dcf056dd073d1dcf6
superalbondiga <bernard.cartes@gmail.com>
2020-06-04 14:27:08 -0300

    se mueven funciones a user controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,27 @@ const pool = require("../database/dataBase");
 const helpers = require("../lib/helpers");
 
 
-
+//Funcion para listar usuarios
+AuthCtrl.listaUsuarios = async (req, res) => {
+    //  const usuarios = await dbHelper.ListarUsuario();
+    const usuarios = await pool.query("select * from usuarios");
+  
+    console.log(usuarios);
+  
+    res.json(usuarios);
+  };
+  //Funcion para eliminar usuarios
+  AuthCtrl.eliminarUsuarios = async (req, res) => {
+    const { rut_usuario } = req.body;
+    const eliminar = await dbHelper.EliminarUsuarios(rut_usuario);
+    res.json(eliminar);
+  };
+  
+  //Funcion para actualizar usuarios
+  AuthCtrl.actualizarUsuarios = async (req, res) => {
+    const { infoUsuario } = req.body;
+    const actualizar = await dbHelper.ActualizarUsuarios(infoUsuario);
+    res.json(actualizar);
+  };
 
 module.exports = userCtrl;
